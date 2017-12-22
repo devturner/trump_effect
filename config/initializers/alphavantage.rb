3313f994d6296fc910caff028c0c7d052daa8b91
Chris Turner <mr.chris.turner+dev@gmail.com>
2017-12-21 21:39:42 -0800

    Getting the initilizers working
    Getting alpha and twitter clients setup for usage.

--- config/initializers/alphavantage.rb
+++ config/initializers/alphavantage.rb
@@ -1,10 +1,10 @@
 require 'alphavantagerb'
 
-@config_path = Rails.root.join('config/alpha.yml').to_s
+@alpha_config_path = Rails.root.join('config/alpha.yml').to_s
 
-if File.exist?(@config_path)
- 	alpha_config = YAML.load_file(@config_path).with_indifferent_access
-	alpha_client = Alphavantage::Client.new key: alpha_config[:key]
+if File.exist?(@alpha_config_path)
+ 	alpha_config = YAML.load_file(@alpha_config_path).with_indifferent_access
+	@alpha_client = Alphavantage::Client.new key: alpha_config[:key]
 else
   # Rails.logger.warn(%Q(Desk configuration not found! No file at "#{config_path}".))
   puts (%Q(Alphavantage configuration not found! No file at "#{config_path}".))
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,11 +1,11 @@
 require 'twitter'
 
-@config_path = Rails.root.join('config/twitter.yml').to_s
+@twitter_config_path = Rails.root.join('config/twitter.yml').to_s
 
-twitter_client = Twitter::REST::Client.new do |config|
+@twitter_client = Twitter::REST::Client.new do |config|
 
-	if File.exist?(@config_path)
-    twitter_config = YAML.load_file(@config_path).with_indifferent_access
+	if File.exist?(@twitter_config_path)
+    twitter_config = YAML.load_file(@twitter_config_path).with_indifferent_access
 
   config.consumer_key        = twitter_config[:consumer_key]
   config.consumer_secret     = twitter_config[:consumer_secret]
@@ -17,3 +17,4 @@ twitter_client = Twitter::REST::Client.new do |config|
     puts (%Q(Twitter configuration not found! No file at "#{config_path}".))
   end
 end
+
