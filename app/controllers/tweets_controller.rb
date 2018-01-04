8a788b2ed8f1f6b374719d5d7fb02cde37ab4cd2
cturner <mr.chris.turner@gmail.com>
2018-01-03 21:40:55 -0800

    stumping out ideas

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
-  def index
-  end
+
+	def index
+	end
+
+	def self.create
+		@tweets = Tweet.new(params[:tweets])
+		
+		@twitter_request = CreateTwitterReport.new(tweets_params).call
+	end
+
 end
