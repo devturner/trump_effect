bd6411e8940fdb7bab48b5d4e4851c879a80e6c8
cturner <mr.chris.turner@gmail.com>
2018-01-03 21:39:36 -0800

    some clean up
    just fixing some indents, etc..

--- app/services/create_twitter_report.rb
+++ app/services/create_twitter_report.rb
@@ -5,9 +5,9 @@
 class CreateTwitterReport
 
 def initialize(params)
-	@tweets
-	@number_of_tweets
-	@created_times = Array.new
+  @tweets = []
+  @number_of_tweets = 30
+  @created_times = []
 end
 
 def call
@@ -16,24 +16,24 @@ def call
   fix_times(@created_times)
 end
 
-private
+  private
 
-def get_tweets(quantity)
-	@tweets = @twitter_client.user_timeline('realDonaldTrump', count: @number_of_tweets)
-end
+    def get_tweets(quantity)
+      @tweets = @twitter_client.user_timeline('realDonaldTrump', count: quantity)
+    end
 
-def tweet_times(collected_tweets)
-	@created_times = @tweets.map { |tweet| tweet.created_at }
-end
+    def tweet_times(collected_tweets)
+      @created_times = @tweets.map { |tweet| tweet.created_at }
+    end
 
-def fix_times(tweet_times)
-	@corrected_times = Array.new
-	for i in @created_times
-		i = i.floor_to(1*60) + Time.now.in_time_zone('America/New_York').utc_offset
-		@corrected_times << i
-	end
-	# market_times(@corrected_times)
-end
+    def fix_times(tweet_times)
+      @corrected_times = Array.new
+      for i in @created_times
+        i = i.floor_to(1*60) + Time.now.in_time_zone('America/New_York').utc_offset
+        @corrected_times << i
+      end
+      # market_times(@corrected_times)
+    end
 
 
 end
