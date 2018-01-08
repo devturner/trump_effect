55f2addd0340d7e0669dc6b0de399f8f9f1eee3f
cturner <mr.chris.turner@gmail.com>
2018-01-08 11:37:27 -0800

    collect new tweets
    collect new tweets I don't have in my DB already.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,24 @@
 class Tweet < ApplicationRecord
 
 	def self.get_latest_trump_tweets
-	    
+		@twitter_client.user_timeline('realDonaldTrump', count: 200).each do |tweet_results|
 
+		#parsing the string 'created_at' to DateTime object
+      twitter_created_at = DateTime.parse(tweet_results.created_at)
+
+		#making sure we are not saving exact same tweet from a person again 
+      unless Tweet.exists?(['twitter_created_at = ? AND from_user_id_str = ?', DateTime.parse(tweet_results.created_at), tweet_results.from_user_id_str])
+
+		#finally creating the tweet record
+        Tweet.create!({
+        	"from_user_id":        tweet["user"]["id"],
+        	"from_user_name":      tweet["user"]["name"],
+        	"tweet_url":           "https://twitter.com/i/web/status/#{tweet["id_str"]}",
+        	"tweet_full_text":     tweet["full_text"],
+        	"tweet_created_at":    tweet["created_at"]
+        })
+      end
+    end
 	end
 
 end
