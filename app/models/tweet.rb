5b4ef9e9b208b2de3cff8dadfc5de690e5527709
cturner <mr.chris.turner@gmail.com>
2018-01-17 22:21:18 -0800

    Working tweet collector
    this works and makes sure that the db is updated with the most recent 100 tweets, however, this should be a cron job, so I need get Whenever Gem working and set that up.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,17 +3,16 @@ class Tweet < ActiveRecord::Base
   validates :tweet_id, uniqueness: true, on: :create
 
     def self.get_latest_trump_tweets
-      x = TwitterAPI.new.client
-      x.user_timeline('realDonaldTrump', count: 200).each do | tweet_results |
-        # unless validates_uniqueness_of tweet_results.id :scope => :tweet_id
-          Tweet.create({
-            "from_user_id":        tweet_results.user.id,
-            "from_user_name":      tweet_results.user.name,
-            "tweet_url":           "https://twitter.com/i/web/status/#{tweet_results.id_str}",
-            "tweet_full_text":     tweet_results.full_text,
-            "tweet_created_at":    tweet_results.created_at,
-            "tweet_id":            tweet_results.id
-          })
+      client = TwitterAPI.new.client
+      client.user_timeline('realDonaldTrump', count: 100, tweet_mode: "extended").each do | tweet_result |
+        Tweet.create({
+          "from_user_id":        tweet_result.user.id,
+          "from_user_name":      tweet_result.user.name,
+          "tweet_url":           "https://twitter.com/i/web/status/#{tweet_result.id.to_s}",
+          "tweet_full_text":     tweet_result.attrs[:full_text],
+          "tweet_created_at":    tweet_result.created_at,
+          "tweet_id":            tweet_result.id
+        })
       end
     end
 
